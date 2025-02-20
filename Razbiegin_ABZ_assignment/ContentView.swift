8f144787f759b5211fcb02e9e4250a03199db428
assistman <razbeginz@gmail.com>
2025-02-20 17:00:28 +0200

    Project created
    Created a project named Razbiegin_ABZ_assignment

---
+++ Razbiegin_ABZ_assignment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Razbiegin_ABZ_assignment
+//
+//  Created by Stanislav Razbiegin on 20.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Razbiegin_ABZ_assignment/Razbiegin_ABZ_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Razbiegin_ABZ_assignmentApp.swift
+//  Razbiegin_ABZ_assignment
+//
+//  Created by Stanislav Razbiegin on 20.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Razbiegin_ABZ_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
